eb2c91db3cadb198b52fba4a680b03aac1b0ebfd
goodman <topdeveloper01234@gmail.com>
2024-02-02 22:26:59 +0800

    fixed: slide line image

--- components/cardetails/CarCarousel.tsx
+++ components/cardetails/CarCarousel.tsx
@@ -142,24 +142,26 @@ const CarCarousel = () => {
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <Image
-                    className="absolute top-[10%] z-0 w-full h-[300px]"
-                    src="/assets/images/car-ellipse.svg"
-                    alt="car"
-                    width={100}
-                    height={30}
-                />
-               
+
                 {/* <CarouselPrevious className="bg-[#424242] hover:bg-primary" /> */}
                 {/* <CarouselNext className="bg-[#424242]" /> */}
-                <div className="w-[100px] m-auto relative top-3">
-                    <div className="flex items-center justify-between bg-white dark:bg-[#2a2b2d] px-4 py-2 rounded-full gap-3">
-                        <CustomPrevious>
-                            <MdKeyboardArrowLeft className="text-2xl text-black dark:text-slate-50" />
-                        </CustomPrevious>
-                        <CustomNext>
-                            <MdKeyboardArrowRight className="text-2xl text-black dark:text-slate-50" />
-                        </CustomNext>
+                <div className=" w-full flex relative">
+                    <Image
+                        className="absolute bottom-3 z-0 w-full h-[100px] sm:h-[300px]"
+                        src="/assets/images/car-ellipse.png"
+                        alt="car"
+                        width={1500}
+                        height={300}
+                    />
+                    <div className="w-[100px] m-auto relative top-3">
+                        <div className="flex items-center justify-between bg-white dark:bg-[#2a2b2d] px-4 py-2 rounded-full gap-3">
+                            <CustomPrevious>
+                                <MdKeyboardArrowLeft className="text-2xl text-black dark:text-slate-50" />
+                            </CustomPrevious>
+                            <CustomNext>
+                                <MdKeyboardArrowRight className="text-2xl text-black dark:text-slate-50" />
+                            </CustomNext>
+                        </div>
                     </div>
                 </div>
             </Carousel>
